3ccd0aeb9c9b7a6ae7347755b28701f7401bb651
AngryFennec <shellfix@gmail.com>
2019-05-22 19:33:14 +0300

    mock

---
+++ src/mocks/offers.js
@@ -0,0 +1,37 @@
+const offers = [
+  {
+    type: `Private room`,
+    price: `80`,
+    rate: 4,
+    src: `img.jpg`,
+    premium: `no`,
+    description: `Wood and stone place`
+  },
+  {
+    type: `Apartment`,
+    price: `120`,
+    rate: 5,
+    src: `img.jpg`,
+    premium: `yes`,
+    description: `Beautiful luxurious apartment at great location`
+  },
+  {
+    type: `Apartment`,
+    price: `132`,
+    rate: 4,
+    src: `img.jpg`,
+    premium: `no`,
+    description: `Nice, cozy, warm big bed apartment`
+  },
+  {
+    type: `Apartment`,
+    price: `180`,
+    rate: 4.5,
+    src: `img.jpg`,
+    premium: `yes`,
+    description: `Canal View Prinsengracht`
+  }
+];
+
+
+export default offers;
